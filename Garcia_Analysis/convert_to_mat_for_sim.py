b9e5da421fb37f409954e9055a7dd597b3a0bf0a
Veronika Wendler <wendler.vroni@gmail.com>
2025-10-30 13:20:14 +0000

    Update convert_to_mat_for_sim.py

--- Garcia_Analysis/convert_to_mat_for_sim.py
+++ Garcia_Analysis/convert_to_mat_for_sim.py
@@ -8,12 +8,11 @@ csv_file = r"D:/Aberdeen_Uni_June24/cap/THESIS/Garcia_Analysis/data/data_sets/Ga
 mat_file = r"D:/Aberdeen_Uni_June24/cap/THESIS/Garcia_Analysis/data/data_sets/Garcia_Eye_for_Simulation.mat"
 
 # Load the CSV file into a Pandas DataFrame
-df = pd.read_csv(csv_file, sep=',')  # Adjust delimiter if needed
+df = pd.read_csv(csv_file, sep=',') 
 
 # Convert DataFrame columns to a dictionary of lists (MATLAB-compatible table structure)
 mat_table = {col: df[col].values for col in df.columns}
 
-# Save to .mat file as a proper MATLAB table
+# Save to .mat file as a MATLAB table
 sio.savemat(mat_file, {"TB": {"Garcia": mat_table}})
-
 print(f"MAT file saved successfully at: {mat_file}")
